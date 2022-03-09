01f2d99f0f69010bffae49be0c265a1c7b2390ed
Noldia Chavez <noldia12@gmail.com>
2022-03-09 16:50:23 -0500

    path_operations_structure_users

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from datetime import date
 from datetime import datetime
 from typing import Optional
+from typing import List
 
 #Pydantic
 from pydantic import BaseModel
@@ -11,6 +12,7 @@ from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 
 app = FastAPI()
@@ -22,12 +24,14 @@ class UserBase(BaseModel):
     user_id: UUID = Field(...)
     email: EmailStr = Field(...)
 
+
 class UserLogin(UserBase):
     password: str = Field(
         ...,
         min_length=1,
         max_length=64
     )
+
 
 class User(UserBase):
     first_name: str = Field(
@@ -57,7 +61,78 @@ class Tweet(BaseModel):
     )
     
 
+# Path Operations
+
 @app.get(path="/")
 def home():
     return {"Twitter API": "Working!!"}
 
+
+## Users
+
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+)
+def signup():
+    pass
+
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=["Users"]
+)
+def Login():
+    pass
+
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all users",
+    tags=["Users"]
+)
+def show_all_users():
+    pass
+
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a User",
+    tags=["Users"]
+)
+def show_a_user():
+    pass
+
+
+@app.delete(
+    path="/signup/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a User",
+    tags=["Users"]
+)
+def delete_a_user():
+    pass
+
+
+@app.put(
+    path="/signup/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Update a User",
+    tags=["Users"]
+)
+def update_a_user():
+    pass
+
+## Tweets
